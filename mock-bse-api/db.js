ed35451c88c0f7c406f1e0dce2bf7463f2ee252c
Prasad Patil <pp5370762@gmail.com>
2026-07-23 00:29:56 +0530

    first commit

--- mock-bse-api/db.js
+++ mock-bse-api/db.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/mock_bse', {
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mock_bse")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
 
-})
-  .then(() => 
-    console.log('MongoDB connected...')
-)
-  .catch(err => 
-    console.error('MongoDB connection error:', err)
-);
+module.exports = mongoose;
